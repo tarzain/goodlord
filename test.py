7878996860f1d77c8b2f3ade35f0686ea5f6c9e8
Zain Shah <zan2434@gmail.com>
2023-11-04 16:45:23 -0700

    starting with semaphore stuff

--- test.py
+++ test.py
@@ -28,120 +28,123 @@ CHANNELS = 1
 RATE = 16000
 CHUNK = 1000
 
-AI_SPEAKING = False
-
 # Set OpenAI API key
 openai.api_key = OPENAI_API_KEY
 
 start_time = time.time()
 
-def is_installed(lib_name):
-    return shutil.which(lib_name) is not None
-
-
-async def text_chunker(chunks):
-    """Split text into chunks, ensuring to not break sentences."""
-    splitters = (".", ",", "?", "!", ";", ":", "—", "-", "(", ")", "[", "]", "}", " ")
-    buffer = ""
-
-    async for text in chunks:
-        if buffer.endswith(splitters):
-            yield buffer + " "
-            buffer = text
-        elif text.startswith(splitters):
-            yield buffer + text[0] + " "
-            buffer = text[1:]
-        else:
-            buffer += text
-
-    if buffer:
-        yield buffer + " "
-
-
-async def stream(audio_stream):
-    """Stream audio data using mpv player."""
-    if not is_installed("mpv"):
-        raise ValueError(
-            "mpv not found, necessary to stream audio. "
-            "Install instructions: https://mpv.io/installation/"
-        )
-
-    mpv_process = subprocess.Popen(
-        ["mpv", "--no-cache", "--no-terminal", "--", "fd://0"],
-        stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
-    )
-
-    print("Started streaming audio")
-    async for chunk in audio_stream:
-        if chunk:
-            mpv_process.stdin.write(chunk)
-            mpv_process.stdin.flush()
-    
-    if mpv_process.stdin:
-        mpv_process.stdin.close()
-    mpv_process.wait()
-
-
-async def text_to_speech_input_streaming(voice_id, text_iterator):
-    """Send text to ElevenLabs API and stream the returned audio."""
-    uri = f"wss://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream-input?model_id=eleven_monolingual_v1"
-
-    async with websockets.connect(uri) as websocket:
-        await websocket.send(json.dumps({
-            "text": " ",
-            "voice_settings": {"stability": 0.5, "similarity_boost": True},
-            "xi_api_key": ELEVENLABS_API_KEY,
-            "optimize_streaming_latency": 3,
-        }))
-
-        async def listen():
-            """Listen to the websocket for audio data and stream it."""
-            while True:
-                try:
-                    message = await websocket.recv()
-                    data = json.loads(message)
-                    if data.get("audio"):
-                        AI_SPEAKING = True
-                        yield base64.b64decode(data["audio"])
-                    elif data.get('isFinal'):
-                        AI_SPEAKING = False
-                        break
-                except websockets.exceptions.ConnectionClosed:
-                    print("Connection closed")
-                    break
-
-        listen_task = asyncio.create_task(stream(listen()))
-
-        async for text in text_chunker(text_iterator):
-            await websocket.send(json.dumps({"text": text, "try_trigger_generation": True}))
-
-        await websocket.send(json.dumps({"text": ""}))
-
-        await listen_task
-
-
-async def chat_completion(query):
-    """Retrieve text from OpenAI and pass it to the text-to-speech function."""
-    response = await openai.ChatCompletion.acreate(
-        model='gpt-4', messages=[{'role': 'user', 'content': query}],
-        temperature=1, stream=True
-    )
-
-    async def text_iterator():
-        async for chunk in response:
-            delta = chunk['choices'][0]["delta"]
-            if 'content' in delta:
-                yield delta["content"]
-            else:
-                break
-        print("generating text took", time.time() - start_time, "seconds")
-
-    await text_to_speech_input_streaming(VOICE_ID, text_iterator())
 
 
 async def run_loop():
     audio_queue = asyncio.Queue()
     start_event = asyncio.Event()  # Create an Event
+    semaphore = asyncio.Semaphore(1)  # Create a Semaphore
+
+
+    def is_installed(lib_name):
+        return shutil.which(lib_name) is not None
+
+
+    async def text_chunker(chunks):
+        """Split text into chunks, ensuring to not break sentences."""
+        splitters = (".", ",", "?", "!", ";", ":", "—", "-", "(", ")", "[", "]", "}", " ")
+        buffer = ""
+
+        async for text in chunks:
+            if buffer.endswith(splitters):
+                yield buffer + " "
+                buffer = text
+            elif text.startswith(splitters):
+                yield buffer + text[0] + " "
+                buffer = text[1:]
+            else:
+                buffer += text
+
+        if buffer:
+            yield buffer + " "
+
+
+    async def stream(audio_stream):
+        """Stream audio data using mpv player."""
+        if not is_installed("mpv"):
+            raise ValueError(
+                "mpv not found, necessary to stream audio. "
+                "Install instructions: https://mpv.io/installation/"
+            )
+
+        mpv_process = subprocess.Popen(
+            ["mpv", "--no-cache", "--no-terminal", "--", "fd://0"],
+            stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+        )
+
+        print("Started streaming audio")
+        async for chunk in audio_stream:
+            if chunk:
+                mpv_process.stdin.write(chunk)
+                mpv_process.stdin.flush()
+        
+        if mpv_process.stdin:
+            mpv_process.stdin.close()
+        mpv_process.wait()
+
+
+    async def text_to_speech_input_streaming(voice_id, text_iterator):
+        """Send text to ElevenLabs API and stream the returned audio."""
+        uri = f"wss://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream-input?model_id=eleven_monolingual_v1"
+
+        async with websockets.connect(uri) as websocket:
+            await websocket.send(json.dumps({
+                "text": " ",
+                "voice_settings": {"stability": 0.5, "similarity_boost": True},
+                "xi_api_key": ELEVENLABS_API_KEY,
+                "optimize_streaming_latency": 3,
+            }))
+
+            async def listen():
+                """Listen to the websocket for audio data and stream it."""
+                while True:
+                    try:
+                        message = await websocket.recv()
+                        data = json.loads(message)
+                        if data.get("audio"):
+                            if semaphore.locked():
+                                semaphore.release()
+                            yield base64.b64decode(data["audio"])
+                        elif data.get('isFinal'):
+                            if not semaphore.locked():
+                                await semaphore.acquire()
+                            break
+                    except websockets.exceptions.ConnectionClosed:
+                        print("Connection closed")
+                        break
+
+            listen_task = asyncio.create_task(stream(listen()))
+
+            async for text in text_chunker(text_iterator):
+                await websocket.send(json.dumps({"text": text, "try_trigger_generation": True}))
+
+            await websocket.send(json.dumps({"text": ""}))
+
+            await listen_task
+
+
+    async def chat_completion(query):
+        """Retrieve text from OpenAI and pass it to the text-to-speech function."""
+        response = await openai.ChatCompletion.acreate(
+            model='gpt-4', messages=[{'role': 'user', 'content': query}],
+            temperature=1, stream=True
+        )
+
+        async def text_iterator():
+            async for chunk in response:
+                delta = chunk['choices'][0]["delta"]
+                if 'content' in delta:
+                    yield delta["content"]
+                else:
+                    break
+            print("generating text took", time.time() - start_time, "seconds")
+
+        await text_to_speech_input_streaming(VOICE_ID, text_iterator())
 
     # Used for microphone streaming only.
     def mic_callback(input_data, frame_count, time_info, status_flag):
@@ -198,10 +201,10 @@ async def run_loop():
     
             try:
                 while True:
-                    if(AI_SPEAKING):
+                    mic_data = await audio_queue.get()
+                    if semaphore.locked():
                         await ws.send(json.dumps({ "type": "KeepAlive" }))
                     else:
-                        mic_data = await audio_queue.get()
                         await ws.send(mic_data)
             except KeyboardInterrupt as _:
                 await ws.send(json.dumps({
